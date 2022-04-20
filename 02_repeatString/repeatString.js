86cd1518ee457efa338e1b3b5cdb50862a6efc3c
jasperjones <jasperjones01@protonmail.com>
2022-04-20 16:51:24 -0500

    Repeat Strings test passed.
    Been able to pass all repeat strings test.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
-};
-
-// Do not edit below this line
-module.exports = repeatString;
+const repeatString = function(word, numberOfTimes) {
+    let addedWords = ''
+    if (numberOfTimes<0){
+        return 'ERROR';
+    }
+   for (var i=0; i<numberOfTimes; i++){
+         addedWords += word;
+   }
+   return addedWords;
+ };
+ 
+ // Do not edit below this line
+ module.exports = repeatString;
