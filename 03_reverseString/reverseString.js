8169e412a9951e423d45077429da1e28f3c7d613
jasperjones <jasperjones01@protonmail.com>
2022-04-26 09:53:15 -0500

    Reverse String assignment complete
    It is a great learning curve and i realize things piecing up together in bits.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
-};
+const reverseString = function(myArrary) {
+    let toBeReversed = myArrary.split('');
+    let reversedArrary = '';
+    for (let i = toBeReversed.length-1; i>=0; i--){
+        reversedArrary += toBeReversed[i];
+        }
+        return reversedArrary;
+    };
 
 // Do not edit below this line
 module.exports = reverseString;
