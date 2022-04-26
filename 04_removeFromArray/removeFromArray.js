6cd8aae37fffca882763949f6021ce7171572e8e
jasperjones <jasperjones01@protonmail.com>
2022-04-26 17:02:32 -0500

    RemoveFromArrary assignment done
    All test passed. I love the learning curve.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
 const removeFromArray = function() {
-
+    let myArrary = arguments[0];
+    for (let i=0; i<myArrary.length; i++){
+        for (let j=0; j<arguments.length; j++){      
+            let index = myArrary.indexOf(arguments[j]);      
+            if (index !== -1) {
+            const removed = myArrary.splice(index, 1);
+            }
+        };
+        return myArrary;
+    }; 
+    
 };
 
 // Do not edit below this line
