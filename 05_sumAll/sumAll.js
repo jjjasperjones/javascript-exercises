39b2c8739ddc6783e66e4278ec6beab35764dc96
jasperjones <jasperjones01@protonmail.com>
2022-05-03 09:38:27 -0500

    completed sumAll and leapYears project
    leapYears was quite challenging because I started function form a different angle.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(firstNumber, secondNumber) {
+    let numberAdd = 0
+    if (firstNumber<0 ||
+       secondNumber<0){
+            return 'ERROR';
+    }
+     if (typeof firstNumber !== `number` || 
+        typeof secondNumber !== `number`) {
+            return 'ERROR';
+    }
+    if (firstNumber<secondNumber){
+    for (let i=firstNumber; i<=secondNumber; i++){
+          numberAdd += i;}
+    }
+    if (firstNumber>secondNumber){
+    for (let i=secondNumber; i<=firstNumber; i++){
+          numberAdd += i;}
+    }
+    return numberAdd;
+  };
 
-};
 
 // Do not edit below this line
 module.exports = sumAll;
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(leapYear) {
+  
+    if (Number.isInteger(leapYear/4) &&
+        !(Number.isInteger(leapYear/100)) ||
+        Number.isInteger(leapYear/400)) {
+            return true;
+    } 
+    else {return false};
 };
 
 // Do not edit below this line
