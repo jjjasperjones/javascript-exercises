0098b71e78897dd75d924dbbfc9e75eb69fd370f
jasperjones <jasperjones01@protonmail.com>
2022-05-05 03:36:51 -0500

    completed calculator and palidromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  var total = 0;
+  for (var i in a) {
+  total += a[i];
+}
+return total;
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+  var total = 1;
+  for (var i in a) {
+  total *= a[i];
+}
+return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let factor = 1;
+  for (let i=a; i>0; i--){
+          factor *= i;}
+    return factor;
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (palindrome) {
+    let toBeReversed = palindrome.split('');
+    let reversedPalindrome = '';
+    for (let i = toBeReversed.length-1; i>=0; i--){
+        reversedPalindrome += toBeReversed[i];
+        }
+    console.log (reversedPalindrome);
+    if (palindrome.toUpperCase().replace(/[^A-Za-z0-9\s]/g,"").replace(/ /g, '') === 
+        reversedPalindrome.toUpperCase().replace(/[^A-Za-z0-9\s]/g,"").replace(/ /g, '')){
+      return true;
+    }
+    else {return false};
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
